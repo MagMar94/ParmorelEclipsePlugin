5454f27588353649ec286b898744a4d633ad29d0
Magnus Marthinsen <magmar@online.no>
2019-10-02 14:24:09 +0200

    able to take in simple preferences and set them to specified values. No cheking for validity

--- src/hvl/plugin/parmorel/handlers/RepairHandler.java
+++ src/hvl/plugin/parmorel/handlers/RepairHandler.java
@@ -1,13 +1,29 @@
 package hvl.plugin.parmorel.handlers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.core.commands.IHandlerListener;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.dialogs.ListSelectionDialog;
+
+import hvl.projectparmorel.ml.QLearning;
 
 public class RepairHandler implements IHandler {
+
+	private String shorterSequences = "Prefer shorter sequences of actions";
+	private String longerSequences = "Prefer longer sequences of actions";
+	private String higherInContext = "Prefer repairing higher in the context hierarchies";
+	private String lowerInContext = "Prefer repairing lower in the context hierarchies";
+	private String rewardModification = "Prefer modification of the original model";
+	private String punishModification = "Punish modification of the original model";
+	private String punishDeletion = "Punish deletion";
 
 	@Override
 	public void addHandlerListener(IHandlerListener handlerListener) {
@@ -24,8 +40,55 @@ public class RepairHandler implements IHandler {
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		Shell shell = new Shell();
-		MessageDialog.openInformation(shell, "Info", "Hello!");
+		String[] options = { shorterSequences, longerSequences, higherInContext, lowerInContext, rewardModification,
+				punishModification, punishDeletion };
+		ListSelectionDialog dialog = new ListSelectionDialog(shell, options, ArrayContentProvider.getInstance(),
+				new LabelProvider(), "selection message");
+		dialog.setTitle("Select preferences");
+		dialog.setInitialSelections(new Object[] {});
+		dialog.open();
+		Object[] selection = dialog.getResult();
+		if (null == selection) {
+			MessageDialog.openWarning(shell, "Warning", "You did not select anything!");
+		} else {
+			String[] result = new String[selection.length];
+			System.arraycopy(selection, 0, result, 0, selection.length);
+//			MessageDialog.openInformation(shell, "Info", "You have selected" + Arrays.toString(result));
+
+			List<Integer> preferences = getPreferencesFrom(result);
+			QLearning qLearning = new QLearning(preferences);
+			
+
+		}
+
 		return null;
+	}
+
+	private List<Integer> getPreferencesFrom(String[] stringPreferences) {
+		List<Integer> preferences = new ArrayList<>();
+		for (String prefOption : stringPreferences) {
+			int preference = getPreferenceNumberFrom(prefOption);
+			preferences.add(preference);
+		}
+		return preferences;
+	}
+
+	private int getPreferenceNumberFrom(String preferenceAsString) {
+		if(preferenceAsString.equals(shorterSequences))
+			return 0;
+		if(preferenceAsString.equals(longerSequences))
+			return 1;
+		if(preferenceAsString.equals(higherInContext))
+			return 2;
+		if(preferenceAsString.equals(lowerInContext))
+			return 3;
+		if(preferenceAsString.equals(rewardModification))
+			return 6;
+		if(preferenceAsString.equals(punishModification))
+			return 5;
+		if(preferenceAsString.equals(punishDeletion))
+			return 4;
+		return -1;
 	}
 
 	@Override
