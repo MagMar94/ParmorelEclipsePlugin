50d0cd42eccd62ac219e7ff58cadd75a43dce82e
Magnus Marthinsen <magmar@online.no>
2019-10-11 10:11:33 +0200

    fixed so a comparison view now appears after the model is fixed. It uses discouraged access on a compare configuration, but I was not able to find any other way of setting up the comparison.

--- src/hvl/plugin/parmorel/handlers/RepairHandler.java
+++ src/hvl/plugin/parmorel/handlers/RepairHandler.java
@@ -6,13 +6,34 @@ import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.compare.CompareConfiguration;
+import org.eclipse.compare.CompareEditorInput;
+import org.eclipse.compare.CompareUI;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.core.commands.IHandlerListener;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.util.BasicMonitor;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.compare.Comparison;
+import org.eclipse.emf.compare.Diff;
+import org.eclipse.emf.compare.EMFCompare;
+import org.eclipse.emf.compare.domain.ICompareEditingDomain;
+import org.eclipse.emf.compare.domain.impl.EMFCompareEditingDomain;
+import org.eclipse.emf.compare.ide.ui.internal.configuration.EMFCompareConfiguration;
+import org.eclipse.emf.compare.ide.ui.internal.editor.ComparisonEditorInput;
+import org.eclipse.emf.compare.merge.BatchMerger;
+import org.eclipse.emf.compare.merge.IBatchMerger;
+import org.eclipse.emf.compare.merge.IMerger;
+import org.eclipse.emf.compare.scope.DefaultComparisonScope;
+import org.eclipse.emf.compare.scope.IComparisonScope;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
+import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.IStructuredSelection;
@@ -29,8 +50,8 @@ public class RepairHandler implements IHandler {
 
 	private String shorterSequences = "Prefer shorter sequences of actions";
 	private String longerSequences = "Prefer longer sequences of actions";
-	private String higherInContext = "Prefer repairing higher in the context hierarchies";
-	private String lowerInContext = "Prefer repairing lower in the context hierarchies";
+	private String higherInContext = "Prefer repairing higher in the hierarchies";
+	private String lowerInContext = "Prefer repairing lower in the hierarchies";
 	private String rewardModification = "Prefer modification of the original model";
 	private String punishModification = "Punish modification of the original model";
 	private String punishDeletion = "Punish deletion";
@@ -62,7 +83,7 @@ public class RepairHandler implements IHandler {
 		String[] options = { shorterSequences, longerSequences, higherInContext, lowerInContext, rewardModification,
 				punishModification, punishDeletion };
 		ListSelectionDialog dialog = new ListSelectionDialog(shell, options, ArrayContentProvider.getInstance(),
-				new LabelProvider(), "selection message");
+				new LabelProvider(), "Preference options:");
 		dialog.setTitle("Select preferences");
 		dialog.setInitialSelections(new Object[] {});
 		dialog.open();
@@ -84,11 +105,10 @@ public class RepairHandler implements IHandler {
 	 * 
 	 * @return the created duplicate
 	 */
-	private File createDuplicateFileFromSelected() {
-		IFile file = getSelectedFile();
-		File destinationFile = new File(file.getLocation().toFile().getParent() + "_temp_" + file.getName());
+	private File createDuplicateFileFrom(File selectedFile) {
+		File destinationFile = new File(selectedFile.getParent() + "_temp_" + selectedFile.getName());
 		try {
-			Files.copy(file.getLocation().toFile().toPath(), destinationFile.toPath());
+			Files.copy(selectedFile.toPath(), destinationFile.toPath());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -103,11 +123,14 @@ public class RepairHandler implements IHandler {
 	private void fixSelectedModelWith(List<Integer> preferences) {
 		modelFixer.setPreferences(preferences);
 		
-		File destinationFile = createDuplicateFileFromSelected();
+		File file = getSelectedFile();
+		File destinationFile = createDuplicateFileFrom(file);
 		URI uri = URI.createFileURI(destinationFile.getAbsolutePath());
 		Resource model = modelFixer.getModel(uri);
 		
 		modelFixer.fixModel(model, uri);
+		
+		compare(file, destinationFile);
 	}
 	
 	private List<Integer> getPreferencesFrom(String[] stringPreferences) {
@@ -137,18 +160,74 @@ public class RepairHandler implements IHandler {
 		return -1;
 	}
 	
-	private IFile getSelectedFile() {
+	private File getSelectedFile() {
 		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
 	    if (window != null)
 	    {
 	        IStructuredSelection selection = (IStructuredSelection) window.getSelectionService().getSelection();
 	        Object firstElement = selection.getFirstElement();
 	        if(firstElement instanceof IFile) {
-	        	return (IFile) firstElement;
+	        	IFile selectedFile = (IFile) firstElement;
+	        	return selectedFile.getLocation().toFile();
 	        }
 	    }
 	    return null;
 	}
+
+	private void compare(File model1, File model2) {
+		URI uri1 = URI.createFileURI(model1.getAbsolutePath());
+		URI uri2 = URI.createFileURI(model2.getAbsolutePath());
+
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("ecore", new EcoreResourceFactoryImpl());
+
+		ResourceSet resourceSet1 = new ResourceSetImpl();
+		ResourceSet resourceSet2 = new ResourceSetImpl();
+
+		resourceSet1.getResource(uri1, true);
+		resourceSet2.getResource(uri2, true);
+
+		CompareConfiguration configuration = new CompareConfiguration();
+		
+		//ComparisonEditorInput
+		IComparisonScope scope = new DefaultComparisonScope(resourceSet1, resourceSet2, null);
+		Comparison comparison = EMFCompare.builder().build().compare(scope);
+		
+		ICompareEditingDomain editingDomain = EMFCompareEditingDomain.create(resourceSet1, resourceSet2, null);
+		AdapterFactory adapterFactory = new ComposedAdapterFactory(ComposedAdapterFactory.Descriptor.Registry.INSTANCE);
+	    CompareEditorInput input = new ComparisonEditorInput(new EMFCompareConfiguration(configuration), comparison, editingDomain, adapterFactory);
+
+//		CompareEditorInput input = new ComparisonEditorInput(new CompareConfiguration(), comparison, scope);
+		
+		 CompareUI.openCompareDialog(input); // or CompareUI.openCompareEditor(input);
+//		List<Diff> differences = comparison.getDifferences();
+//		// Let's merge every single diff
+//		IMerger.Registry mergerRegistry = new IMerger.RegistryImpl();
+//		IBatchMerger merger = new BatchMerger(mergerRegistry);
+//		merger.copyAllLeftToRight(differences, new BasicMonitor());
+		
+	}
+	
+	
+//	public void compare(Notifier left, Notifier right, Notifier ancestor) {
+//	    EMFCompare comparator = EMFCompare.builder().build();
+//	    Comparison comparison = comparator.compare(EMFCompare.createDefaultScope(left, right, ancestor));
+//
+//	    ICompareEditingDomain editingDomain = EMFCompareEditingDomain.create(left, right, ancestor);
+//	    AdapterFactory adapterFactory = new ComposedAdapterFactory(ComposedAdapterFactory.Descriptor.Registry.INSTANCE);
+//	    CompareEditorInput input = new CompareEditorInput(new CompareConfiguration(), comparison, editingDomain, adapterFactory);
+//
+//	   
+//	}
+//	private void compare(Notifier left, Notifier right, Notifier ancestor) {
+//	    EMFCompare comparator = EMFCompare.builder().build();
+//	    Comparison comparison = comparator.compare(new DefaultComparisonScope(left, right, ancestor));
+//
+//	    ICompareEditingDomain editingDomain = EMFCompareEditingDomain.create(left, right, ancestor);
+//	    AdapterFactory adapterFactory = new ComposedAdapterFactory(ComposedAdapterFactory.Descriptor.Registry.INSTANCE);
+//	    CompareEditorInput input = new ComparisonEditorInput(new CompareConfiguration(), comparison, editingDomain, adapterFactory);
+//
+//	    CompareUI.openCompareDialog(input); // or CompareUI.openCompareEditor(input);
+//	}
 
 	@Override
 	public boolean isEnabled() {
