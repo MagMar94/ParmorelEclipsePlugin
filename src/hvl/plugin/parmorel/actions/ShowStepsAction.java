dbe1c252da02d98d6bd4709ce880f0acc47ef1f8
Magnus Marthinsen <magmar@online.no>
2019-10-30 12:39:56 +0100

    handled error dialogs better now that I'm reading about dialogs. The stack trace will show.

--- src/hvl/plugin/parmorel/actions/ShowStepsAction.java
+++ src/hvl/plugin/parmorel/actions/ShowStepsAction.java
@@ -6,6 +6,8 @@ import org.eclipse.jface.action.Action;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredViewer;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 
 import hvl.projectparmorel.modelrepair.AppliedAction;
 import hvl.projectparmorel.modelrepair.Solution;
@@ -26,12 +28,29 @@ public class ShowStepsAction extends Action {
 		if (obj instanceof Solution) {
 			Solution solution = (Solution) obj;
 			List<AppliedAction> steps = solution.getSequence();
-			// show steps
+			displaySteps(steps);
 		} else {
+			displaySteps(null);
 			showError("An error occured when retrieving the solution.");
 		}
 	}
 
+	private void displaySteps(List<AppliedAction> steps) {
+		Display display = new Display ();
+		Shell shell = new Shell (display);
+		shell.setText ("Shell");
+		shell.setSize (200, 200);
+		shell.open ();
+		Shell dialog = new Shell (shell);
+		dialog.setText ("Dialog");
+		dialog.setSize (200, 200);
+		dialog.open ();
+		while (!shell.isDisposed ()) {
+			if (!display.readAndDispatch ()) display.sleep ();
+		}
+		display.dispose ();
+	}
+	
 //	private void showMessage(String message) {
 //		MessageDialog.openInformation(viewer.getControl().getShell(), POP_UP_HEADLINE, message);
 //	}
