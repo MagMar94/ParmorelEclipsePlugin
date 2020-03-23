87d061fce2d496159d22b00b999055346aa1a972
Magnus Marthinsen <magmar@online.no>
2020-03-23 10:37:58 +0100

    Displays that the distance is not available when the distance could not be calculated

--- src/hvl/plugin/parmorel/views/DistanceLabelProvider.java
+++ src/hvl/plugin/parmorel/views/DistanceLabelProvider.java
@@ -4,12 +4,16 @@ import org.eclipse.jface.viewers.ColumnLabelProvider;
 
 import hvl.projectparmorel.modelrepair.Solution;
 
-public class DistanceLabelProvider extends ColumnLabelProvider {
+public class DistanceLabelProvider extends ColumnLabelProvider {	
 	@Override
 	public String getText(Object element) {
-		if(element != null && element instanceof Solution) {
+		if (element != null && element instanceof Solution) {
 			Solution solution = (Solution) element;
-			return Double.toString(solution.calculateDistanceFromOriginal());
+			double distance = solution.calculateDistanceFromOriginal();
+			if (distance < 0) {
+				return "Not available";
+			}
+			return Double.toString(distance);
 		}
 		return "";
 	}
